2eb5481459b7c221f77ce52de24e61cff8c9146d
Vasanth B <vasanthb112003@gmail.com>
2026-05-23 13:17:33 +0530

    Update work.js

--- work.js
+++ work.js
@@ -1,6 +1,18 @@
 const apiKey = "d1b9b5d72eec457b081e6d83eb18facf";
 
-function getWeather() {
+let timer;
+
+// Debouncing Function
+function debounceWeather() {
+    clearTimeout(timer);
+
+    timer = setTimeout(() => {
+        getWeather();
+    }, 1000); // waits 1 second
+}
+
+// Async/Await Function
+async function getWeather() {
     const city = document.getElementById("cityInput").value;
 
     if (city === "") {
@@ -10,16 +22,31 @@ function getWeather() {
 
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
-    fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            document.getElementById("city").innerText = data.name;
-            document.getElementById("temp").innerText = "Temperature: " + data.main.temp + "°C";
-            document.getElementById("desc").innerText = "Weather: " + data.weather[0].description;
-            document.getElementById("humidity").innerText = "Humidity: " + data.main.humidity + "%";
-            document.getElementById("wind").innerText = "Wind Speed: " + data.wind.speed + " m/s";
-        })
-        .catch(error => {
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+
+        // Check city valid or not
+        if (data.cod !== 200) {
             alert("City not found!");
-        });
+            return;
+        }
+
+        document.getElementById("city").innerText = data.name;
+
+        document.getElementById("temp").innerText =
+            "Temperature: " + data.main.temp + "°C";
+
+        document.getElementById("desc").innerText =
+            "Weather: " + data.weather[0].description;
+
+        document.getElementById("humidity").innerText =
+            "Humidity: " + data.main.humidity + "%";
+
+        document.getElementById("wind").innerText =
+            "Wind Speed: " + data.wind.speed + " m/s";
+
+    } catch (error) {
+        alert("Something went wrong!");
+    }
 }
